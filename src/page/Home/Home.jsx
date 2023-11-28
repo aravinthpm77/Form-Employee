452981346b78232ebc8669da049eb5952db889ff
Aravinthpm <aravinth7703@gmail.com>
2023-11-28 23:01:00 +0530

    front End Complete

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Home.css'
-import img1 from '../../assets/bg1.jpg'
+import img1 from '../../assets/bg3.jpg'
 const Home = () =>{
     return (
         <div className='form-con'> 
-            <div className='text-area'>
-                <img src={img1} alt="Image1"  width='200px' height='150px'/>
-            </div>  
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="date" />
-                <input type="text" placeholder="Dept you worked"/>
-                <input type="text" placeholder="Address"/>
-                <input type="number" placeholder="Empy-ID"/>
-                <input type="number" placeholder="Salary Details"/>
-                <input type="text" placeholder="Destination"/>
-                <button type="button" >Submit</button>
-            </form>
+            <div className='form-box'>
+                <div className='text-area'>
+                    <img src={img1} alt="Image1"  />
+                </div>  
+                <form>
+                    <div className='inpute'>
+                        <input type="text" placeholder="Name" />
+                        <input type="date" />
+                        <input type="text" placeholder="Dept you worked"/>
+                        <input type="text" placeholder="Address"/>
+                        <input type="number" placeholder="Empy-ID"/>
+                        <input type="number" placeholder="Salary Details"/>
+                        <input type="text" placeholder="Destination"/>
+                        <button type="button" >Submit</button>
+                    </div>
+                    
+                </form>
+            </div>
+            
         </div>
     )
 }
